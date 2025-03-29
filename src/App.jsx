8a22b23694f39bf433a4041877d941d13d2f113f
2AWebDeveloper <ali.webdev2002@gmail.com>
2025-03-29 20:29:42 +0330

    create Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
   return (
     <ThemeProvider>
       <Header />
+      <Main></Main>
     </ThemeProvider>
   );
 }
